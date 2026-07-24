77c65c611b8c5233e28e16b7e99864d5b8cfe127
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-24 14:42:56 +0900

    赤荻 viテスト追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
+const apiBaseUrl =
+  process.env.API_BASE_URL ??
+  "http://74.176.217.130";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "trainingstorage20260713.blob.core.windows.net",
+        port: "",
+        pathname:
+          "/product-images/products/**",
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        /**
+         * 顧客アカウント登録API
+         */
+        source:
+          "/proxy-api/account/:path*",
+        destination:
+          `${apiBaseUrl}/account/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
