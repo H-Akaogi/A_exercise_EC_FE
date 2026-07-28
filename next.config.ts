cf74f3f162e9693cfe083d913c38c146c994a8dd
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-28 13:54:37 +0900

    赤荻 接続先変更

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-import type { NextConfig } from "next";
+import type {
+  NextConfig,
+} from "next";
 
-// const apiBaseUrl =
-//   process.env.API_BASE_URL ??
-//   "http://74.176.217.130";
-
-const apiBaseUrl =
+const apiBaseUrl = (
   process.env.API_BASE_URL ??
-  "http://localhost:5100";
+  "http://127.0.0.1:5100"
+).replace(/\/+$/, "");
 
 const nextConfig: NextConfig = {
   output: "standalone",
@@ -27,81 +26,54 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        /**
-         * UC002 顧客ログインAPI
-         */
         source:
           "/proxy-api/login",
         destination:
           `${apiBaseUrl}/login`,
       },
       {
-        /**
-         * UC008 顧客ログアウトAPI
-         */
         source:
           "/proxy-api/logout",
         destination:
           `${apiBaseUrl}/logout`,
       },
       {
-        /**
-         * 顧客アカウント登録API
-         */
         source:
           "/proxy-api/account/:path*",
         destination:
           `${apiBaseUrl}/account/:path*`,
       },
       {
-        /**
-         * 商品一覧API
-         */
         source:
           "/proxy-api/product/:path*",
         destination:
           `${apiBaseUrl}/product/:path*`,
       },
       {
-        /**
-         * 商品一覧API
-         */
         source:
           "/proxy-api/products/:path*",
         destination:
           `${apiBaseUrl}/products/:path*`,
       },
       {
-        /**
-         * 購入API
-         */
         source:
           "/proxy-api/purchase/complete/:path*",
         destination:
           `${apiBaseUrl}/purchase/complete/:path*`,
       },
       {
-        /**
-         * 商品一覧API
-         */
         source:
           "/proxy-api/purchase/history/:path*",
         destination:
           `${apiBaseUrl}/purchase/history/:path*`,
       },
       {
-        /**
-         * カテゴリ一覧API
-         */
         source:
           "/proxy-api/product-category/options",
         destination:
           `${apiBaseUrl}/product-category/options`,
       },
       {
-        /**
-         * 支払方法一覧API
-         */
         source:
           "/proxy-api/payment-method/options",
         destination:
