2852bbad7683983d41fe96b0c8e2377b47218302
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-29 14:19:31 +0900

    赤荻 トップ画面名前

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { TopPage } from "@/components/purchase/TopPage";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "トップページ",
+  title: "トップ | フルネス文具",
 };
 export default function HomePage() {
   return <TopPage />;
